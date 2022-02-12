de4f225532f51651e79dc4c586e1b6fe5c58b61f
stephenavocado <stephenwalter@chicagobooth.edu>
2022-02-12 23:51:24 +0000

    font import

--- App.js
+++ App.js
@@ -2,9 +2,21 @@ import React from "react";
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurant.screen";
 import { ThemeProvider } from "styled-components/native";
-import { theme } from "./src/infrastructure/theme";
+import { theme } from "./src/infrastructure/theme/index";
+
+import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald';
+import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
+
 
 export default function App() {
+
+  const [oswaldLoaded] = useFonts({ Oswald_400Regular });
+  const [latoLoaded] = useFonts({ Lato_400Regular });
+
+  if(!oswaldLoaded || !latoLoaded){
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
