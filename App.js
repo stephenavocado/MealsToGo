3d097de6521564b054519ced9d4b2d7b7bce480b
stephenavocado <stephenwalter@chicagobooth.edu>
2022-02-12 15:07:22 -0600

    restaurant info

--- App.js
+++ App.js
@@ -1,44 +1,12 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { 
-  StatusBar, 
-  StyleSheet, 
-  Text, 
-  View, 
-  SafeAreaView, 
-  Platform 
-} from 'react-native';
-import { Searchbar } from "react-native-paper";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurant.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-            <Searchbar />
-        </View>
-        <View style={styles.list}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style='auto' />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, 
-    marginTop: StatusBar.currentHeight
-  },
-  search: {
-    padding: 16, 
-    backgroundColor: "white"
-  },
-  list: {
-    flex: 1, 
-    padding: 16, 
-    backgroundColor: "blue"
-  }
-})
-
---
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { 
+  StatusBar, 
+  StyleSheet, 
+  Text, 
+  View, 
+  SafeAreaView, 
+  Platform 
+} from 'react-native';
+import { RestaurantInfo } from "../components/restaurant-info.component";
+import { Searchbar } from "react-native-paper";
+
+export const RestaurantsScreen = () => {
+    return(
+        <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+            <Searchbar />
+        </View>
+        <View style={styles.list}>
+          <RestaurantInfo />
+        </View>
+      </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        marginTop: StatusBar.currentHeight
+    },
+    search: {
+        padding: 16, 
+        backgroundColor: "white"
+    },
+    list: {
+        flex: 1, 
+        padding: 16, 
+        backgroundColor: "blue"
+    }
+})
