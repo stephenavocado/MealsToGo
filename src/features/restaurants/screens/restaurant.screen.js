b05b27db7dbd64d19c1ece9dc5a712817f6e0b7a
stephenavocado <stephenwalter@chicagobooth.edu>
2022-02-12 22:13:15 +0000

    migrated to styled components

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -10,32 +10,33 @@ import {
 } from 'react-native';
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { Searchbar } from "react-native-paper";
+import styled from "styled-components/native";
+
+const Container = styled(SafeAreaView)`
+    flex: 1;
+    marginTop: ${StatusBar.currentHeight}px;
+`;
+
+const SearchContainer = styled.View`
+    padding: 16px; 
+    backgroundColor: white;
+`
+
+const ListContainer = styled.View`
+    flex: 1;
+    padding: 16px; 
+    backgroundColor: blue;
+`
 
 export const RestaurantsScreen = () => {
     return(
-        <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-            <Searchbar />
-        </View>
-        <View style={styles.list}>
-          <RestaurantInfoCard />
-        </View>
-      </SafeAreaView>
+        <Container>
+            <SearchContainer>
+                <Searchbar />
+            </SearchContainer>
+            <ListContainer>
+                <RestaurantInfoCard />
+            </ListContainer>
+        </Container>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1, 
-        marginTop: StatusBar.currentHeight
-    },
-    search: {
-        padding: 16, 
-        backgroundColor: "white"
-    },
-    list: {
-        flex: 1, 
-        padding: 16, 
-        backgroundColor: "blue"
-    }
-})
