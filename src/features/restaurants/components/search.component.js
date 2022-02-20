09ddbfa5212188feef068e987f32fdfcdc177cbd
stephenavocado <stephenwalter@chicagobooth.edu>
2022-02-20 19:46:47 +0000

    search bar compoonent

---
+++ src/features/restaurants/components/search.component.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Searchbar } from "react-native-paper";
+
+import { LocationContext } from "../../../services/location/location.context"
+
+const SearchContainer = styled.View`
+    padding: ${(props) => props.theme.space[3]}; 
+`
+
+export const Search = () => {
+    const locationContext = useContext(LocationContext);
+    console.log(locationContext); 
+    return (
+        <SearchContainer>
+            <Searchbar />
+        </SearchContainer>
+    );
+};
