6ef96d1a7ffecf1ca5ff073bf9c07d7e241321dc
stephenavocado <stephenwalter@chicagobooth.edu>
2022-02-20 23:40:37 +0000

    created ssearchbar component with location context

---
+++ src/features/restaurants/components/search.component.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import styled from "styled-components/native";
+import { Searchbar } from "react-native-paper";
+
+import { LocationContext } from "../../../services/location/location.context";
+
+const SearchContainer = styled.View`
+    padding: ${(props) => props.theme.space[3]}; 
+`
+
+export const Search = () => {
+    const locationContext = useContext(LocationContext);
+    console.log(locationContext);
+
+    return (
+        <SearchContainer>
+            <Searchbar />
+        </SearchContainer>
+    ) 
+}
