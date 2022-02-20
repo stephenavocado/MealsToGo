7e0c7dcd07cb542db4f6fa743687eef7e5c5da2d
stephenavocado <stephenwalter@chicagobooth.edu>
2022-02-20 02:18:45 +0000

    yarn add camelize

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,4 +1,5 @@
 import { mocks } from "./mock";
+import camelize from "camelize"
 
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
     return new Promise((resolve, reject) => {
