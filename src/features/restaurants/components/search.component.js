02312e36f38d4e4da6c1327e254db0f28a9fd3f1
stephenavocado <stephenwalter@chicagobooth.edu>
2022-02-21 00:33:00 +0000

    strigger search render cycle

--- src/features/restaurants/components/search.component.js
+++ src/features/restaurants/components/search.component.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import styled from "styled-components/native";
 import { Searchbar } from "react-native-paper";
 
@@ -11,6 +11,10 @@ const SearchContainer = styled.View`
 export const Search = () => {
     const { keyword, search } = useContext(LocationContext);
     const [searchKeyword, setSearchKeyword] = useState(keyword);
+
+    useEffect(() => {
+        search(searchKeyword);
+    }, []);
 
     return (
         <SearchContainer>
--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -1,4 +1,5 @@
-import React, {useState, useEffect, useMemo, createContext } from "react";
+import React, {useState, useEffect, useMemo, createContext, useContext } from "react";
+import { LocationContext } from "../location/location.context";
 
 import { restaurantsRequest, restaurantsTransform } from "./restaurants.service";
 
@@ -8,11 +9,13 @@ export const RestaurantsContextProvider = ({ children }) => {
     const [restaurants, setRestaurants] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
+    const { location } = useContext(LocationContext);
 
-    const retrieveRestaurants = () => {
+    const retrieveRestaurants = (loc) => {
         setIsLoading(true);
+        setRestaurants([]);
         setTimeout(() => {
-            restaurantsRequest()
+            restaurantsRequest(loc)
                 .then(restaurantsTransform)
                 .then((results) => {
                     setIsLoading(false);
@@ -25,8 +28,11 @@ export const RestaurantsContextProvider = ({ children }) => {
     }
 
     useEffect(() => {
-        retrieveRestaurants();
-    }, []);
+        if (location) {
+        const locationString = `${location.lat},${location.lng}`
+        retrieveRestaurants(locationString);
+        };
+    }, [location]);
 
     return (
         <RestaurantsContext.Provider
