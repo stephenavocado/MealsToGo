d7f61df24f4c9a96584f32a5eacffa48b3f0a190
stephenavocado <stephenwalter@chicagobooth.edu>
2022-02-21 22:04:29 +0000

    favourites-bar

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { FlatList, TouchableOpacity } from 'react-native';
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { ActivityIndicator, Colors } from "react-native-paper";
@@ -6,6 +6,7 @@ import styled from "styled-components/native";
 import { SafeArea } from "../../../components/utility/safe-area.component"
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { FavouritesContext } from "../../../services/favourites/favourites.context";
+import { FavouritesBar } from "../../../components/favourites/favourites-bar.component";
 
 import { Search } from "../components/search.component"
 import { Spacer } from "../../../components/spacer/spacer.component";
@@ -29,6 +30,7 @@ const LoadingContainer = styled.View`
 export const RestaurantsScreen = ({ navigation }) => {
     const { isLoading, error, restaurants } = useContext(RestaurantsContext);
     const { favourites } = useContext(FavouritesContext);
+    const [isToggled, setIsToggled] = useState(false);
 
     return(
         <SafeArea>
@@ -37,7 +39,11 @@ export const RestaurantsScreen = ({ navigation }) => {
                     <Loading size={50} animating={true} color={Colors.blue300} />
                 </LoadingContainer>
             )}
-            <Search />
+            <Search 
+                isFavouritesToggled={isToggled} 
+                onFavouritesToggle={() => setIsToggled(!isToggled)} 
+            />
+            {isToggled && <FavouritesBar favourites={favourites} onNavigate={navigation.navigate} /> }
             <RestaurantList 
                 data={restaurants}
                 renderItem={({ item }) => {
