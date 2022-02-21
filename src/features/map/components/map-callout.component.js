c8e627682ee2e24c48fb66c9ffb9cf5d033670a7
stephenavocado <stephenwalter@chicagobooth.edu>
2022-02-21 18:49:28 +0000

    linkable callouts

--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -1,10 +1,9 @@
 import React from "react";
-import styled from "styled-components/native";
 
-const MyText = styled.Text``;
+import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info.component"
 
 export const MapCallout = ({ restaurant }) => {
     return (
-        <MyText>{restaurant.name}</MyText>
+        <CompactRestaurantInfo restaurant={restaurant} />
     )
 };
--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -12,7 +12,7 @@ const Map = styled(MapView)`
     width: 100%;
 `
 
-export const MapScreen = () => {
+export const MapScreen = ({ navigation }) => {
 
 const { location } = useContext(LocationContext);
 const { restaurants = [] } = useContext(RestaurantsContext);
@@ -48,7 +48,12 @@ return (
                             longitude: restaurant.geometry.location.lng,
                         }}
                     >
-                        <MapView.Callout>
+                        <MapView.Callout
+                            onPress={() => navigation.navigate("RestaurantDetail", { 
+                                restaurant,
+                                })
+                            }
+                        >
                             <MapCallout restaurant={restaurant} />
                         </MapView.Callout>
                     </MapView.Marker> ;
